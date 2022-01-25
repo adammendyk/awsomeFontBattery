b4b8e6511bf4b1fb182d2ba5b39eb774683b4d97
adammendyk <adammendyk@yahoo.com>
2022-01-25 11:07:37 +0100

    bckp

--- awsomefont_battery_icon.py
+++ awsomefont_battery_icon.py
@@ -1,17 +1,15 @@
 """
-FontAwsome icons to be integrated with qtile battery widget to show energy levels
-along with orginal widget text.
+FontAwsome icons integrated with qtile battery widget to show energy levels.
 Battery name is set to BAT0 by default. To change it add:
 widget.afBatteryIcon(
     battery="BAT#"
 )
 # - being number of battery you want to add (0, 1, 2...)
-
 """
 
-from time import sleep
 from libqtile.widget import base
-# from time import sleep
+
+BAT_NAME = "BAT0"
 
 
 class afBatteryIcon(base._TextBox):
@@ -20,7 +18,7 @@ class afBatteryIcon(base._TextBox):
     defaults = [
         (
             "battery",
-            "BAT0",
+            BAT_NAME,
             "System battery name (BAT0, BAT1, BAT2...)"
         ),
         (
@@ -42,6 +40,26 @@ class afBatteryIcon(base._TextBox):
             "update_interval",
             30,
             "Update interval seconds."
+        ),
+        (
+            "alert",
+            5,
+            "Alert battery level."
+        ),
+        (
+            "font",
+            "FontAwsome",
+            "FontAwsome is mandatory to properly display icons."
+        ),
+        (
+            "foreground",
+            "#f3f4f5",
+            "Icon colour"
+        ),
+        (
+            "alert_colour",
+            "#cd1f3f",
+            "Colour when empty."
         )
     ]
 
@@ -50,17 +68,18 @@ class afBatteryIcon(base._TextBox):
         self.add_defaults(afBatteryIcon.defaults)
         self.capacity = self._get_battery_capacity()
         self.status = self._get_battery_status()
+        self.icon = self.set_icon()
 
     def _get_battery_capacity(self):
         with open(f"/sys/class/power_supply/{self.battery}/capacity") as bat:
-            return int((bat.readlines())[0])
+            return int((bat.readlines())[0].strip())
 
     def _get_battery_status(self):
         with open(f"/sys/class/power_supply/{self.battery}/status") as bat:
             return (bat.readlines())[0].strip()
 
     def set_icon(self):
-        capacity_value = self.capacity
+        current_capacity = self.capacity
         current_status = self.status
 
         # Function constants
@@ -74,22 +93,31 @@ class afBatteryIcon(base._TextBox):
         }
         # Ranges
         status_ico = ""
-        if capacity_value <= 100:
+        if current_capacity <= 100:
             status_ico = ICONS["full"]
-        if capacity_value <= 75:
+        if current_capacity <= 75:
             status_ico = ICONS["three-quarters"]
-        if capacity_value <= 50:
+        if current_capacity <= 50:
             status_ico = ICONS["half"]
-        if capacity_value <= 25:
+        if current_capacity <= 25:
             status_ico = ICONS["quarter"]
-        if capacity_value <= 5:
+        if current_capacity <= 5:
             status_ico = ICONS["empty"]
         if current_status == "Charging":
             status_ico = ICONS["charging"]
         self.icon = status_ico
+        return status_ico
+
+    def draw(self):
+        if self.capacity <= self.alert:
+            self.layout.colour = self.alert_colour
+        else:
+            self.layout.colour = self.foreground
+        base.ThreadPoolText.draw(self)
 
     def poll(self):
-        return self.set_icon()
+        icon = self.set_icon()
+        return icon
 
     # def __repr__(self):
     #     return f"{self.capacity} = {self.icon}"
